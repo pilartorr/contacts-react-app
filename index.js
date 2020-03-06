e66e004e457720bcdf8809b5432122867299d457
pilartorr <sauleturme@gmail.com>
2020-03-06 14:45:00 +0100

    testing hello world

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
+const path = require('path');
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });

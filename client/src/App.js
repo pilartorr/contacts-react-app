de4db0a47ff6366c80f443ffa91ac790957d43a2
pilartorr <sauleturme@gmail.com>
2020-03-10 14:40:30 +0100

    created the state to remove contacts of the list

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,43 @@ import React, { Component } from 'react';
 import './index.css';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-  }
-];
-
 class App extends Component {
+  state = {
+    contacts: [
+        {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "karen_isgrigg",
+          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "richardkalehoff",
+          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "tylermcginnis",
+          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+        }
+    ]
+  }
+  removeContact = contactToDelete => {
+    this.setState(currentState => ({
+      contacts: currentState.contacts.filter(contact => {
+        return contact.id !== contactToDelete.id
+      })
+    }))
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts}/>
+        <ListContacts 
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
       </div>
     );
   }

b809bf408cf5c813226b2c047e8b5ff6f3784286
pilartorr <sauleturme@gmail.com>
2020-03-23 12:07:03 +0100

    connected with Contacts API and created CreateContact Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,77 @@
-import React, { Component } from 'react';
-import './index.css';
-import ListContacts from './ListContacts';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
     contacts: [
-        {
-          "id": "karen",
-          "name": "Karen Isgrigg",
-          "handle": "karen_isgrigg",
-          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-        },
-        {
-          "id": "richard",
-          "name": "Richard Kalehoff",
-          "handle": "richardkalehoff",
-          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-        },
-        {
-          "id": "tyler",
-          "name": "Tyler McGinnis",
-          "handle": "tylermcginnis",
-          "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
-        }
+      {
+        "id": "karen",
+        "name": "Karen Isgrigg",
+        "handle": "karen_isgrigg",
+        "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+      },
+      {
+        "id": "richard",
+        "name": "Richard Kalehoff",
+        "handle": "richardkalehoff",
+        "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "https://i.pinimg.com/originals/21/89/24/218924160f2ed76d531b31c3ff4e30ca.jpg"
+      }
     ]
   }
-  removeContact = contactToDelete => {
-    this.setState(currentState => ({
-      contacts: currentState.contacts.filter(contact => {
-        return contact.id !== contactToDelete.id
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
       })
     }))
+
+    ContactsAPI.remove(contact)
+  }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
   }
   render() {
     return (
       <div>
-        <ListContacts 
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-        />
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const path = require('path');
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT);
